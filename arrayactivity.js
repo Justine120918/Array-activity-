c8f9859b333446b9dd1231cb2555ed54f22f81c2
Justine120918 <camarillocombisjustine@gmail.com>
2024-10-17 10:34:12 +0000

    Initial commit with app.js

---
+++ arrayactivity.js
@@ -0,0 +1,11 @@
+let favSingers = ["Singer1", "Singer2", "Singer3"];
+
+console.log(favSingers[0]);
+
+let favNumbers = [7, 14, 21, 28];
+let mixedArr = ["string", ["otherarray"], 123, true];
+
+console.log(mixedArr[0]); // Logs: "string"
+console.log(mixedArr[1]); // Logs: ["otherarray"]
+console.log(mixedArr[2]); // Logs: 123
+console.log(mixedArr[3]); // Logs: true
